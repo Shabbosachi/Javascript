28bc05cf5a7409b8557cd5ca6300448fe96c80d6
Shabbosachi Das <sabokdas@gmail.com>
2019-01-21 07:28:17 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+// console.log('hi');
+// let x = 7;
+// let y = 8;
+// let z = x + y;
+// console.log('Answer is: ' + z);
+
+// let x = 7;
+// console.log(typeof x);
+
+// let y = true;
+// console.log(typeof y);
+
+// let z = 'love u';
+// console.log(typeof z);
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+
+// let a = 7;
+// let b = '8';
+// b = parseInt(b, 10);
+// let c = a + b;
+// console.log('Result:' + c);
+
+// let d = parseInt('sobbo', 10);
+// let e = isNaN(d);
+// console.log(d);
+// console.log(e);
+
+//Operators
+
+//Assignmet =
+//Arithmetic + - * / %
+// var m = 20 % 3
+// console.log(m);
+//Increment / Decrement ++ --
+// var a =1;
+// a++;
+// console.log(a++);
+// console.log(a);
+
+// let a = [4, 5, 6, 7, 8, 9, 10];
+// let b = ['sabbir', 'sena', 'Biva', 'sohel', 'antora'];
+// console.log(b);
+// console.log(b[0]);
+// console.log(b[1]);
+// a[0] = 7;
+// console.log(a);
+// console.log(typeof a);
+// console.log(a.length); //is not zero based
+// a[10] =77;
+// console.log(a);
+// console.log(a.length);
+// a.push(77);
+// console.log(a);
+// console.log(a.length);
+
+// a.pop();
+// a.pop();
+// console.log(a);
+// console.log(a.length);
+
+
+
+// function sayHello() {
+//     console.log('=================');
+//     console.log('Hello! Welcome.');
+//     console.log('=================');
+// }
+// // sayHello();
+
+// let a = sayHello;
+// a();
+// a();
+// a();
+
+// function calculateTax(amount) {
+//     let result = amount * .0825;
+//     return result;
+// }
+// let tax = calculateTax(100);
+// console.log(tax);
+
+// setTimeout(function() {
+//     console.log('I will wait 5 seconds')
+// },5000);
+
+// let counter = 0;
+// function timeout() {
+//     setTimeout(function(){
+//         console.log('Count ' + counter++);
+//         timeout();
+//     }, 2000);
+// }
+
+// timeout();
+
+// (function () {
+//     console.log('Immediately Invoked Function Expression(IIFE)');
+// })();
+
+// var count = 6;
+// if (count == 4) {
+//     console.log('Count is 4');
+// } else if (count > 4) {
+//     console.log('Count is greater than 4');
+// } else if (count < 4) {
+//     console.log('Count is Less than 4');
+// } else {
+//     console.log('Count is not 4');
+// }
+
---
+++ variables.js
@@ -0,0 +1,8 @@
+// let x = 7;
+// let y = 8;
+// let z = x + y;
+// console.log('Answer is: ' + z);
+
+
+let x = 7;
+console.log(typeof x);
